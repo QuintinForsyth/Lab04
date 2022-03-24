fb3e969cfe6a1d32424bacb9c6d8f00fd8307a7a
Quintin Forsyth <100383529+QuintinForsyth@users.noreply.github.com>
2022-03-24 21:29:53 +0000

    quicksave

---
+++ part2.py
@@ -0,0 +1,30 @@
+import turtle
+import time
+
+
+t = turtle.Turtle()
+
+
+def stairs_down1(x):
+  t.forward(x)
+  t.right(90)
+  t.forward(x)
+  t.left(90)
+  t.forward(x)
+  t.right(90)
+  t.forward(x)
+  t.left(90)
+  t.forward(x)
+  t.right(90)
+  t.forward(x)
+  t.left(90)
+  t.forward(x)
+  t.right(90)
+  t.forward(x)
+  t.left(90)
+  t.forward(x)
+  time.sleep(2)
+  t.home()
+  t.clear()
+
+stairs_down1(20)
