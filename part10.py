b8e13e2167ca8678c40277c7a3fd0ad131319d86
Quintin Forsyth <100383529+QuintinForsyth@users.noreply.github.com>
2022-03-26 06:55:50 +0000

    chess revamped

--- part10.py
+++ part10.py
@@ -12,9 +12,9 @@ import time
 t = turtle.Turtle()
 def teleports (size, count):
     t.penup()
-    t.home()
+    t.home() # uses home to warp back
     t.right(90)
-    t.forward(size*count)
+    t.forward(size*count) # moves down by a size times count of lines
     t.left(90)
     t.pendown()
 
@@ -30,7 +30,7 @@ def draw_square(size):
   t.forward(size)
 
   
-def chess_board(size, total_to_loop,two_per_row):
+def chess_board(size, total_to_loop,two_per_row): # does two rows 
   count = 0
   for i in range(total_to_loop):
     count = count + 1
@@ -58,7 +58,7 @@ def chess_board(size, total_to_loop,two_per_row):
 
 
 
-t.
-chess_board(20,4,4)
+
+chess_board(20,4,4) # draws 4 squares of each time
 turtle.getscreen()._root.mainloop()
 
